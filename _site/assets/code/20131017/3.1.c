d5187362065f8125b5d7c9666c51de5416ead645
freejie <cj@cj.(none)>
2013-11-07 21:04:33 +0800

    *

---
+++ _site/assets/code/20131017/3.1.c
@@ -0,0 +1,26 @@
+int getMaxSubarray(int array[], int len, int *sum, int *low, int *high)
+{
+    int temp;
+    int i;
+
+    temp = *sum = 0;
+    *low = *hith = 0;
+    for (i = 0; i < len; i++)
+    {
+        if (temp < 0)
+        {
+            temp = array[i];
+            *low = i;
+        }
+        else 
+        {
+            temp += array[i];
+            *high = i;
+        }
+        if (*sum < temp)
+            *sum = temp;
+    }
+
+    return 0;
+}
+
---
+++ assets/code/20131017/2.1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <limits.h>
+
+struct 
+void findMaxCrossingSubarray(int array[], int left, int right, int mid)
+{
+    int leftsum = MIN_INT;
+    int rightsum = MIN_INT;
+    int i, j;
+}
+
+void findMaxmimuxSubarray(int array[], int low, int high)
+{
+    int left;
+    int right;
+    int mid;
+
+    if (len <= 0)
+        return;
+    mid = (left + right) / 2;
+    findMaxmimumSubarray(array, left, mid);
+    findMaxmimumSubarray(array, mid+1, right);
+
+
+}
+
+int main(void)
+{
+
+}
